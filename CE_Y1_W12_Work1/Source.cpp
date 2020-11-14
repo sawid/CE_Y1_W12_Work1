1e85e1d8eacba982603fda87489af3f4168b9942
sawid <zasawidza@gmail.com>
2020-11-15 02:01:41 +0700

    Update Bug

--- CE_Y1_W12_Work1/Source.cpp
+++ CE_Y1_W12_Work1/Source.cpp
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-char reverse(char);
+void reverse(char*);
 
 int main() {
 	char str[201];
@@ -9,21 +9,21 @@ int main() {
 	scanf("%[^\n]", str);
 	while (str[i] != '\0')
 	{
-		printf("%c", reverse(str[i]));
+		reverse(&str[i]);
+		printf("%c", str[i]);
 		i++;
 	}
 
 	return 0;
 }
 
-char reverse(char str) {
-	if (str >= 'A' && str <= 'Z')
+void reverse(char* str) {
+	if (*str >= 'A' && *str <= 'Z')
 	{
-		str += 32;
+		*str += 32;
 	}
-	else if (str >= 'a' && str <= 'z')
+	else if (*str >= 'a' && *str <= 'z')
 	{
-		str -= 32;
+		*str -= 32;
 	}
-	return str;
 }
